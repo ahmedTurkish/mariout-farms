e48d3538cbf281d82c12982a1181c9ea76b46876
ahmedTurkish <74664253+ahmedTurkish@users.noreply.github.com>
2025-11-20 16:43:29 +0200

    Update and rename public/js/main.js to public./sections.js

---
+++ public./sections.js
@@ -0,0 +1,28 @@
+// ===== Reveal on Scroll =====
+const scrollElements = document.querySelectorAll(".scroll-reveal");
+
+const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return elementTop <= (window.innerHeight || document.documentElement.clientHeight) - offset;
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("show");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 100)) {
+      displayScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener("scroll", () => {
+  handleScrollAnimation();
+});
+
+// Initial check
+handleScrollAnimation();
+
+
